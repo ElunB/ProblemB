bca9b623e5b34688c216767258fbe24fd394bec4
Elin Bonnedahl <Elin.Bonnedahl@afaforsakring.se>
2021-03-16 06:46:36 +0100

    Add project files.

---
+++ ProblemB/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace ProblemB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ").Cast<int>();
+            var dividedWeight = input.Sum() / 2;
+            var weights = input.ToArray();
+            var numberOfWeights = weights[0];
+            // Ta bort första elementet som bara är antal vikter 
+            weights.ToList().Remove(0);
+           // var sortedList = weights.ToList();
+           // sortedList.Sort();
+
+
+            double backpack1 = 0;
+            double backpack2;
+            int closest = 0;
+
+            for (int i = 0; i < weights.Length; i++)
+            { 
+                // Först testar vi kombinationen index[0] i weights tillsammans med varje enskild vikt i arrayen 
+                // Om vikterna tillsammans inte är större än jämnvikten och den tidigare närmaste vikten så sparar vi vikten som closest tills vi
+                //hittar en mer optimerad kombination 
+                for (int k = 1; k <= dividedWeight; k++)
+                {
+                    k = findClosest(weights, weights[i], i + 1, dividedWeight);
+                    if (k < dividedWeight)
+                    {
+                        closest = k;
+                    }
+                    closest+=  weights[i + 1];
+
+                }
+            }
+            Console.WriteLine(closest);
+
+                }
+
+
+        public static int findClosest(int [] weightArray, int current, int index, int dividedWeight)
+        {
+            int max = current;
+            for(int i = index; i < weightArray.Length; i++)
+            {
+                if(current + weightArray[i] < dividedWeight && current + weightArray[i] > current)
+                {
+                    max = current + weightArray[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test
+{
+    class Program
+    {
+        List<int> results = new List<int>();
+
+        static void Main(string[] args)
+        {
+            var weights = new List<int> { 529, 382, 130, 462, 223, 167, 235, 529 };
+
+            var p = new Program();
+
+            p.NewMethod(weights);
+            p.results.Sort();
+            double sum = weights.Sum();
+            double dividedWeight = sum / 2; 
+            var result = p.results.Where(w => w <= dividedWeight).Max();
+            double result2 = sum - result;
+            Console.WriteLine("dividedweight: " + dividedWeight.ToString());
+            Console.WriteLine("första: "+ result2.ToString() + " andra: " +  result.ToString());
+
+            Console.WriteLine(string.Join(" ", p.results.Distinct()));
+        }
+
+        private void NewMethod(List<int> weights, int sum = 0)
+        { // metodanrop 2: 9 12 3 , 5 
+         // lägger till 5 + 9 = 14 
+         // metodanrop 3: 12 3, 14 
+         // metodanrop 4: 3, 26
+         // metodanrop: 5: tom lista, 29 - foreachen körs inte eftersom listan är tom 
+         
+        //result: 5, 14, 26, 29,   9,    12, 
+            foreach(var weight in weights)
+            {
+                results.Add(weight + sum);
+                NewMethod(weights.Where(w => w != weight).ToList(), weight + sum);
+            }
+        }
+    }
+}
